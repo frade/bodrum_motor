e51bee511019793ca30db3a23648b5b5a7da14ca
frade <fedorof.aa@gmail.com>
2024-08-31 23:50:40 -0700

    150mb

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const port = 3000;
 
 // Increase the limit for JSON payloads
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: '150mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const AI_API_KEY = process.env.AI_API_KEY;
